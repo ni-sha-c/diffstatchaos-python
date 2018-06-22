04d53acf459ef944a4562d986915f3c3ac48fef0
Nisha Chandramoorthy <nishac92@gmail.com>
2018-06-22 11:35:14 -0400

    alg outline 2nd attempt

--- src/flow-sens.py
+++ src/flow-sens.py
@@ -20,28 +20,40 @@ if __name__ == "__main__":
 
 
 
-n_steps = 100000
+n_steps = 20000
 n_runup = 50000
-u = rand(d)
-u = Step(u,s0,n_runup)
-ds = zeros(p)
-dJ = zeros(d)
-v0 = rand(d)
-w0 = rand(d)
-v0 /= norm(v0)
-w0 /= norm(w0)
-ds1 = copy(ds)
-ds1[0] = 1.0
-v = zeros(d)
-for i in arange(n_steps):
-    v0 = tangent_step(v0,u,s0,ds)
-    w0 = adjoint_step(w0,u,s0,dJ)
-    v0 /= norm(v0)
-    w0 /= norm(w0)
-   
-    dfds = DfDs(u,s0)[:,0]
-    dfds,_= decompose_tangent(dfds,v0,w0)
-    v = tangent_step(v,u,s0,dfds)
-    u = Step(u,s0,1) 
+u = zeros((d,n_steps))
+u0 = rand(d)
+u0[3] *= T
+u0 = Step(u0,s0,n_runup)
+u[:,0] = copy(u0)
 
+ds0 = zeros(p)
+dJ0 = zeros(d)
+
+v0 = zeros((d,n_steps))
+w0 = zeros((d,n_steps))
+
+v0[:,0] = rand(d)
+v0[:,0] /= norm(v0[:,0])
+w0[:,n_steps-1] = rand(d)
+w0[:,n_steps-1] /= norm(w0[:,n_steps-1])
+
+v = zeros(d)
+w = zeros(d)
+
+for i in arange(1,n_steps):
+	v0[:,i] = tangent_step(v0[:,i-1],u[:,i-1],s0,ds0)
+	v0[:,i] /= norm(v0[:,i])
+	u[:,i] = Step(u[:,i-1],s0,1) 
+
+for i in arange(n_steps-1,0,-1):
+	w0[:,i-1] = adjoint_step(w0[:,i],u[:,i-1],s0,dJ0)
+	w0[:,i-1] /= norm(w0[:,i-1])
+
+
+for i in arange(1,n_steps-1):
+	dfds = DfDs(u[:,i],s0)[:,0]
+	dfds,_= decompose_tangent(dfds,v0[:,i+1],w0[:,i+1])
+	v = tangent_step(v,u[:,i],s0,ds0) + dfds*dt
 
