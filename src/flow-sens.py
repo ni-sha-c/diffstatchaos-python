dfe85e3e2f15b05a09d2ef6a485f5865e4ee2dc9
Qiqi Wang <qiqi@mit.edu>
2018-06-24 12:45:12 -0700

    Refactored a few loops to prepare for acceleration

--- src/flow-sens.py
+++ src/flow-sens.py
@@ -9,16 +9,8 @@ from kuznetsov import *
 
 from pylab import *
 from numpy import *
-from mpi4py import MPI
 from time import time
 from util import *
-
-if __name__ == "__main__":
-    comm = MPI.COMM_WORLD
-    if comm.rank == 0:
-        print("test")
-
-
 
 n_steps = 100
 n_runup = 10000
@@ -27,6 +19,7 @@ n_bins_phi = 20
 dtheta = pi/n_bins_theta
 dphi = 2*pi/n_bins_phi
 u = zeros((d,n_steps))
+random.seed(0)
 u0 = rand(d)
 u0[3] *= T
 u0 = Step(u0,s0,n_runup)
@@ -44,10 +37,8 @@ w0[:,n_steps-1] = rand(d)
 w0[:,n_steps-1] /= norm(w0[:,n_steps-1])
 
 J_theta_phi = zeros((n_bins_theta,n_bins_phi))
-theta_bin_centers = linspace(dtheta/2.0,pi - dtheta/2.0,
-						n_bins_theta)
-phi_bin_centers = linspace(-pi-dphi/2.0,pi - dphi/2.0,
-						n_bins_phi)
+theta_bin_centers = linspace(dtheta/2.0,pi - dtheta/2.0, n_bins_theta)
+phi_bin_centers = linspace(-pi-dphi/2.0,pi - dphi/2.0, n_bins_phi)
 t_ind = -1
 p_ind = -1
 v = zeros(d)
@@ -57,25 +48,30 @@ divdfds = zeros((p,n_steps))
 dJds_stable = zeros((n_bins_theta,n_bins_phi))
 dJds_unstable = zeros((n_bins_theta,n_bins_phi))
 
-
+# inputs: u[:,0], s0, ds0, v0[:,0]
 for i in arange(1,n_steps):
-    dfds[:,:,i] = DfDs(u[:,i-1],s0)
-    divdfds[:,i] = divDfDs(u[:,i-1],s0) 
     v0[:,i] = tangent_step(v0[:,i-1],u[:,i-1],s0,ds0)
     v0[:,i] /= norm(v0[:,i])
     u[:,i] = Step(u[:,i-1],s0,1)
-    t_ind = -1
-    for theta0 in theta_bin_centers:
-        t_ind += 1
-        p_ind = 0
-        for phi0 in phi_bin_centers:
+
+for i in arange(1,n_steps):
+    dfds[:,:,i] = DfDs(u[:,i-1],s0)
+
+for i in arange(1,n_steps):
+    for t_ind, theta0 in enumerate(theta_bin_centers):
+        for p_ind, phi0 in enumerate(phi_bin_centers):
             J_theta_phi[t_ind,p_ind] += objective(u[:,i-1],
-								s0,theta0,dtheta,phi0,dphi)/n_steps 
-            p_ind += 1
+                    s0,theta0,dtheta,phi0,dphi)/n_steps
+
 for i in arange(n_steps-1,0,-1):
     w0[:,i-1] = adjoint_step(w0[:,i],u[:,i-1],s0,dJ0)
     w0[:,i-1] /= norm(w0[:,i-1])
 
+for i in arange(1,n_steps):
+    divdfds[:,i] = divDfDs(u[:,i-1],s0)
+
+
+'''
 for i in arange(1,n_steps-1):
     source_tangent = DfDs(u[:,i],s0)[:,0]
     source_adjoint = divGradfs(u[:,i],s0)
@@ -116,8 +112,6 @@ for i in arange(n_samples):
             phi0 = phi_bin_centers[i2]
             J_sum_p[i1,i2] += objective(um,s0,theta0,dtheta,phi0,dphi)
             J_sum_m[i1,i2] += objective(um,s0,theta0,dtheta,phi0,dphi)
-            dJds_fd[i1,i2] += (J_sum_p[i1,i2]-J_sum_m[i1,i2])/(2.0*epsi)/n_samples 
-	
-	
-	
-
+            dJds_fd[i1,i2] += (J_sum_p[i1,i2]-J_sum_m[i1,i2]) \
+                            / (2.0*epsi)/n_samples
+'''
