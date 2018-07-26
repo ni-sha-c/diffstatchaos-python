3c2bb3dde2d0ad5ae971e9e69ebd9028a77bafef
Nisha Chandramoorthy <nishac92@gmail.com>
2018-07-26 09:41:40 -0400

    exponential decay for sawtooth and catmap

--- src/driver_fourier.py
+++ src/driver_fourier.py
@@ -6,18 +6,35 @@ from numba import jit
 from matplotlib import *
 from fourierAnalysis import *
 sys.path.insert(0,'../examples/')
-from kuznetsov_poincare import *
+from kuznetsov import *
 
 foa = FourierAnalysis()
-plykin_map_solver = Solver()
-u = foa.solve_primal(plykin_map_solver, \
-        plykin_map_solver.u_init, \
-        1000000, plykin_map_solver.s0)
+solver = Solver()
+u = foa.solve_primal(solver, \
+        solver.u_init, \
+        solver.n_poincare*1000, solver.s0)
 
 u = u.T
+'''
+n_max = 500
+corr_uu = foa.compute_correlation_function(u[0],u[0],n_max) - \
+        mean(u[0])*mean(u[0])
+figure()
+plot(range(1,n_max+1),corr_uu ,".")
+title(r'$\rho_{u,u}}$')
+
+
+dft_corr_uu = (2.0/n_max)*\
+        abs(fft.fft(corr_uu))[:n_max//2]
+figure()
+plot(range(n_max//2),dft_corr_uu)
+title(r'$\hat{\rho}_{u,u}}$')
+
+
+'''
 fig, ax = subplots(nrows=3,ncols=3)
 fig_fft, ax_fft = subplots(nrows=3,ncols=3)
-n_max = 100
+n_max = solver.n_poincare*50
 ax[0,1].axis('off')
 ax[0,2].axis('off')
 ax[1,-1].axis('off')
@@ -33,7 +50,8 @@ for i in range(3):
     f = u[i]
     for j in range(i+1):
         g = u[j]
-        corr_uu[i,j] = foa.compute_correlation_function(f,g,n_max)
+        corr_uu[i,j] = abs(foa.compute_correlation_function(f,g,n_max) \
+                - mean(f)*mean(g))
         ax[i,j].plot(range(1,n_max+1),corr_uu[i,j],'--')
         ax[i,j].set_title(r'$\rho_{%d,%d}}$' %(i,j) )
         
