c6c9edb1140ffb4e3eddc30039ddebd334d9172e
nishaChandramoorthy <nishac92@gmail.com>
2019-03-03 05:54:02 -0500

    cleaner KS

--- examples/KS.py
+++ examples/KS.py
@@ -15,8 +15,9 @@ spec = [
 class Solver:
 
     def __init__(self):
-        self.dt = 2.e-3
-        self.state_dim = 127
+        self.dt = 1.e-1
+        self.L = 8
+        self.state_dim = 7
         self.param_dim = 5
         self.boundaries = ones((2,self.state_dim))        
         self.boundaries[0] = -0.5
@@ -25,20 +26,21 @@ class Solver:
                      self.boundaries[0]) + self.boundaries[0]
         u_init_norm = norm(self.u_init)
         self.u_init /= u_init_norm
-        self.s0 = zero(param_dim)
-        s0[0] = 0.5
+        self.s0 = zeros(self.param_dim)
+        self.s0[0] = 0.5
         self.u_init[-1] = 0.0
 
     
     def primal_step(self,u0,s,n=1):
         u = copy(u0)
         for i in range(n):
-            u += dt*self.primal_vector_field(u,s)
+            u += self.dt*self.primal_vector_field(u,s)
         return u
 
     def primal_vector_field(self,u,s):
         state_dim = u.shape[0]
         dx = self.L/(state_dim+1)
+        c = self.s0[0]
         dx_inv = 1./dx
         dx_inv_sq = dx_inv*dx_inv
         dx_inv_4 = dx_inv_sq*dx_inv_sq
@@ -46,16 +48,82 @@ class Solver:
                 diag(ones(state_dim-1),1)  
         A += (-0.5*c*dx_inv + dx_inv_sq - 4.0*dx_inv_4)*\
                 diag(ones(state_dim-1),-1)
-        A += (-2.0*dx_inv_sq + 6.0*dx_inv**4.0)*\
-                diag(state_dim)
+        A += (-2.0*dx_inv_sq + 6.0*dx_inv_4)*\
+                diag(ones(state_dim))
         A += dx_inv_4*diag(ones(state_dim-2),2)
         A += dx_inv_4*diag(ones(state_dim-2),-2)
         u_sq = u*u
+        print("A is filled up...")
+        print(A)
         Bu = zeros_like(u_sq)
         Bu[1:-1] = u_sq[2:]-u_sq[:-3]
         Bu[0] = u_sq[1]
         Bu[-1] = -u_sq[-2]
         Bu *= 4.0*dx_inv
         dudt = dot(A,u) + Bu
+        print(A)
+        print(Bu)
 
-    
+
+if __name__ == "__main__":
+    dx = 1.
+    state_dim = 7
+    u = rand(7)
+    dx_inv = 1./dx
+    c = 0.5
+    dx_inv_sq = dx_inv*dx_inv
+    dx_inv_4 = dx_inv_sq*dx_inv_sq
+    advection = 0
+    diffusion = 0
+    nonlinear = 0
+    super_diffusion = 0
+    super_diagonal_matrix = \
+            diag(ones(state_dim -1), 1)
+    sub_diagonal_matrix = \
+            diag(ones(state_dim -1), -1)
+    diagonal_matrix = \
+            diag(ones(state_dim))
+    super_super_diagonal_matrix = \
+            diag(ones(state_dim-2), 2)
+    sub_sub_diagonal_matrix = \
+            diag(ones(state_dim-2), -2)
+
+
+    advection_coeff = 0.5*dx_inv
+    diffusion_coeff = dx_inv_sq
+    super_diffusion_coeff = dx_inv_4
+    nonlinear_coeff = 0.25*dx_inv
+
+   
+    super_diagonal_matrix_coeff = (advection*advection_coeff + \
+            diffusion*diffusion_coeff + \
+            super_diffusion*super_diffusion_coeff*(-4.0))
+    sub_diagonal_matrix_coeff = (advection*advection_coeff*(-1) + \
+            diffusion*diffusion_coeff + \
+            super_diffusion*super_diffusion_coeff*(-4.))
+    diagonal_matrix_coeff = (diffusion*diffusion_coeff*(-2.0) + \
+        super_diffusion*super_diffusion_coeff*(6.0))
+    super_super_diagonal_matrix_coeff = \
+            super_diffusion*super_diffusion_coeff
+    sub_sub_diagonal_matrix_coeff = \
+            super_diffusion*super_diffusion_coeff
+
+
+    linear_matrix = super_diagonal_matrix_coeff*super_diagonal_matrix + \
+           sub_diagonal_matrix_coeff*sub_diagonal_matrix + \
+           diagonal_matrix_coeff*diagonal_matrix + \
+           super_super_diagonal_matrix_coeff*super_super_diagonal_matrix + \
+           sub_sub_diagonal_matrix_coeff*sub_sub_diagonal_matrix
+
+    linear_contrib = dot(linear_matrix, u)
+     
+    u_sq = u*u
+    Bu = zeros_like(u_sq)
+    Bu[1:-1] = u_sq[2:]-u_sq[:-2]
+    Bu[0] = u_sq[1]
+    Bu[-1] = -u_sq[-2]
+    Bu *= 4.0*dx_inv
+    dudt = dot(A,u) + Bu
+    print(A)
+    print(Bu)
+
