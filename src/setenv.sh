933ced344de9f707a121d29737a21a17f37e4e34
nishaChandramoorthy <nishac92@gmail.com>
2019-04-04 11:22:20 -0400

    Add unit test to see if CUDA toolkit is working with numba. Add setenv.sh file to set the required environment variables if the CUDA toolkit was installed through deb package and not through conda/directly from nvidia's website.

---
+++ src/setenv.sh
@@ -0,0 +1,4 @@
+export NUMBAPRO_LIBDEVICE=/usr/local/cuda-9.0/nvvm/libdevice/
+export NUMBAPRO_NVVM=/usr/local/cuda-9.0/nvvm/lib64/libnvvm.so
+export NUMBAPRO_CUDALIB=/usr/local/cuda-9.0/nvvm/lib64/
+
