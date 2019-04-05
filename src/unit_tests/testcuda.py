b99dfeb1eac11c4a2e7cf92d088a4aeedd54f1aa
nishaChandramoorthy <nishac92@gmail.com>
2019-04-05 18:48:59 -0400

    writing tridiagonal matrix vector multiply

--- src/unit_tests/testcuda.py
+++ src/unit_tests/testcuda.py
@@ -3,33 +3,38 @@ import numpy as np
 TPB = 16
 
 @cuda.jit
-def matmul(A,B,C):
-	sA = cuda.shared.array(shape=(TPB,TPB), dtype=float64)
-	sB = cuda.shared.array(shape=(TPB,TPB), dtype=float64)
-	x, y = cuda.grid(2)
-	tx = cuda.threadIdx.x
-	ty = cuda.threadIdx.y
-	bpg = cuda.gridDim.x
+def tridvec(u,dx,Au):
+	t = cuda.grid(1)
+	dx_inv = 1/dx
+	dx_inv_2 = dx_inv*dx_inv
+	dx_inv_4 = dx_inv_2*dx_inv_2
+	coeff1 = dx_inv_2 - 4.0*dx_inv_4
+	coeff0 = -2.0*dx_inv_2 + 6.0*dx_inv_4 
+	state_dim = x.shape[0]
+	if t==0:
+		Au[0] = (-2.0*dx_inv_2 + 7.0*dx_inv_4)*u[0] + \
+				coeff1*u[1] + dx_inv_4*u[2]
+	elif t==state_dim-1:
+		Au[t] = (-2.0*dx_inv_2 + 7.0*dx_inv_4)*u[t] + \
+				coeff1*u[t-1] + dx_inv_4*u[t-2]
+	elif t==1:
+		Au[t] = coeff1*u[t+1] + coeff1*u[t-1] + \
+				coeff0*u[t] + dx_inv_4*u[t+2]
+	elif t==state_dim-2:
+		Au[t] = coeff1*u[t+1] + coeff1*u[t-1] + \
+				coeff0*u[t] + dx_inv_4*u[t-2]
+	else:
+		Au[t] = coeff1*u[t+1] + coeff1*u[t-1] + \
+				coeff0*u[t] + dx_inv_4*u[t-2] + \
+				dx_inv_4*u[t+2]
 
-	if x >= C.shape[0] and y >= C.shape[1]:
-		return
-	tmp = 0.
-	for i in range(bpg):
-		sA[tx,ty] = A[x, ty + i*TPB]
-		sB[tx,ty] = B[tx + i*TPB, y]
+
 		
-		cuda.syncthreads()
-		for j in range(TPB):
-			tmp += sA[tx,j]*sB[j,ty]
-		cuda.syncthreads()
-	
-	C[x,y] = tmp
- 
-A = np.random.rand(256,256)
-B = np.random.rand(256,256)
-C = np.empty_like(A)
-threadsperblock = (16, 16)
-blockspergrid_x = np.math.ceil(A.shape[0] / threadsperblock[0])
+tpb = 16
+L = 128
+state_dim = 127
+dx = L/(state_dim + 1)
+bpg = np.math.ceil(A.shape[0] / threadsperblock[0])
 blockspergrid_y = np.math.ceil(A.shape[1] / threadsperblock[1])
 blockspergrid = (blockspergrid_x, blockspergrid_y)
 matmul[blockspergrid, threadsperblock](A,B,C)
