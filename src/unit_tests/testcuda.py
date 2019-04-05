8530814aa7c55f2abdda7f6f8f9634788207f31d
nishaChandramoorthy <nishac92@gmail.com>
2019-04-05 18:02:02 -0400

    Add template files for gpu examples of KS and Kuznetsov poincare.

--- src/unit_tests/testcuda.py
+++ src/unit_tests/testcuda.py
@@ -1,19 +1,35 @@
-from numba import cuda
+from numba import cuda, float64
 import numpy as np
+TPB = 16
 
 @cuda.jit
-def max_example(result, values):
-    """Find the maximum value in values and store in result[0]"""
-    tid = cuda.threadIdx.x
-    bid = cuda.blockIdx.x
-    bdim = cuda.blockDim.x
-    i = (bid * bdim) + tid
-    cuda.atomic.max(result, 0, values[i])
+def matmul(A,B,C):
+	sA = cuda.shared.array(shape=(TPB,TPB), dtype=float64)
+	sB = cuda.shared.array(shape=(TPB,TPB), dtype=float64)
+	x, y = cuda.grid(2)
+	tx = cuda.threadIdx.x
+	ty = cuda.threadIdx.y
+	bpg = cuda.gridDim.x
 
-
-arr = np.random.rand(16384)
-result = np.zeros(1, dtype=np.float64)
-
-max_example[256,64](result, arr)
-print(result[0]) # Found using cuda.atomic.max
-print(max(arr))  # Print max(arr) for comparision (should be equal!)
+	if x >= C.shape[0] and y >= C.shape[1]:
+		return
+	tmp = 0.
+	for i in range(bpg):
+		sA[tx,ty] = A[x, ty + i*TPB]
+		sB[tx,ty] = B[tx + i*TPB, y]
+		
+		cuda.syncthreads()
+		for j in range(TPB):
+			tmp += sA[tx,j]*sB[j,ty]
+		cuda.syncthreads()
+	
+	C[x,y] = tmp
+ 
+A = np.random.rand(256,256)
+B = np.random.rand(256,256)
+C = np.empty_like(A)
+threadsperblock = (16, 16)
+blockspergrid_x = np.math.ceil(A.shape[0] / threadsperblock[0])
+blockspergrid_y = np.math.ceil(A.shape[1] / threadsperblock[1])
+blockspergrid = (blockspergrid_x, blockspergrid_y)
+matmul[blockspergrid, threadsperblock](A,B,C)
