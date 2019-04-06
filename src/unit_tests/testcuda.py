85f7ba8d0d1e5bdeea638b4705a2949d1d8c27fd
nishaChandramoorthy <nishac92@gmail.com>
2019-04-05 20:44:22 -0400

    Rewrote RHS computation on gpu. no of threads = size of state vector

--- src/unit_tests/testcuda.py
+++ src/unit_tests/testcuda.py
@@ -8,33 +8,76 @@ def tridvec(u,dx,Au):
 	dx_inv = 1/dx
 	dx_inv_2 = dx_inv*dx_inv
 	dx_inv_4 = dx_inv_2*dx_inv_2
-	coeff1 = dx_inv_2 - 4.0*dx_inv_4
-	coeff0 = -2.0*dx_inv_2 + 6.0*dx_inv_4 
-	state_dim = x.shape[0]
+	coeff1 = -dx_inv_2 + 4.0*dx_inv_4
+	coeff0 = 2.0*dx_inv_2 - 6.0*dx_inv_4 
+	coeff2 = -dx_inv_4
+	state_dim = u.shape[0]
 	if t==0:
-		Au[0] = (-2.0*dx_inv_2 + 7.0*dx_inv_4)*u[0] + \
-				coeff1*u[1] + dx_inv_4*u[2]
+		Au[0] = (2.0*dx_inv_2 - 7.0*dx_inv_4)*u[0] + \
+				coeff1*u[1] + coeff2*u[2]
 	elif t==state_dim-1:
-		Au[t] = (-2.0*dx_inv_2 + 7.0*dx_inv_4)*u[t] + \
-				coeff1*u[t-1] + dx_inv_4*u[t-2]
+		Au[t] = (2.0*dx_inv_2 - 7.0*dx_inv_4)*u[t] + \
+				coeff1*u[t-1] + coeff2*u[t-2]
 	elif t==1:
 		Au[t] = coeff1*u[t+1] + coeff1*u[t-1] + \
-				coeff0*u[t] + dx_inv_4*u[t+2]
+				coeff0*u[t] + coeff2*u[t+2]
 	elif t==state_dim-2:
 		Au[t] = coeff1*u[t+1] + coeff1*u[t-1] + \
-				coeff0*u[t] + dx_inv_4*u[t-2]
-	else:
+				coeff0*u[t] + coeff2*u[t-2]
+	elif t<=state_dim-1:
 		Au[t] = coeff1*u[t+1] + coeff1*u[t-1] + \
-				coeff0*u[t] + dx_inv_4*u[t-2] + \
-				dx_inv_4*u[t+2]
+				coeff0*u[t] + coeff2*u[t-2] + \
+				coeff2*u[t+2]
 
+@cuda.jit
+def advection(u,dx,s,Bu):
+	t = cuda.grid(1)
+	dx_inv = 1/dx
+	coeffl = -0.5*s*dx_inv  
+	coeffnl = -0.25*dx_inv
+	state_dim = u.shape[0]
+	if t==0:
+		Bu[0] = coeffl*u[1] + coeffnl*u[1]*u[1]
+	elif t==state_dim-1:
+		Bu[t] = -coeffl*u[t-1] - coeffnl*u[t-1]*u[t-1]
+	elif t < state_dim-1:
+		Bu[t] = coeffl*u[t+1] - coeffl*u[t-1] + \
+				coeffnl*u[t+1]*u[t+1] - \
+				coeffnl*u[t-1]*u[t-1]
 
 		
 tpb = 16
-L = 128
-state_dim = 127
+L = 512
+state_dim = 511
 dx = L/(state_dim + 1)
-bpg = np.math.ceil(A.shape[0] / threadsperblock[0])
-blockspergrid_y = np.math.ceil(A.shape[1] / threadsperblock[1])
-blockspergrid = (blockspergrid_x, blockspergrid_y)
-matmul[blockspergrid, threadsperblock](A,B,C)
+bpg = (state_dim + 1) // tpb
+u = np.ones(state_dim)
+s = 1.0
+Au = np.zeros_like(u) 
+tridvec[bpg, tpb](u, dx, Au)
+dx_inv = 1/dx
+dx_inv_2 = dx_inv*dx_inv
+dx_inv_4 = dx_inv_2*dx_inv_2
+coeff0 = 2.0*dx_inv_2 - 6.*dx_inv_4
+coeff1 = -dx_inv_2 + 4.*dx_inv_4
+coeff2 = -dx_inv_4
+A = coeff0*np.diag(np.ones(state_dim),0) + \
+    coeff1*np.diag(np.ones(state_dim - 1),1) + \
+    coeff1*np.diag(np.ones(state_dim - 1),-1) + \
+    coeff2*np.diag(np.ones(state_dim - 2),-2) + \
+    coeff2*np.diag(np.ones(state_dim - 2), 2) 
+A[0,0] += coeff2
+A[-1,-1] += coeff2
+Au_cpu = np.dot(A,u)
+assert(np.linalg.norm(Au - Au_cpu)<=1.e-10)
+Bu = np.zeros_like(u)
+advection[bpg, tpb](u, dx, s, Bu)
+coeffl = 0.5*dx_inv
+coeffnl = 0.25*dx_inv
+B = np.diag(np.ones(state_dim - 1), 1) - \
+    np.diag(np.ones(state_dim - 1), -1)
+u_sq = u*u
+Bu_cpu = s*coeffl*np.dot(B,u) + \
+	coeffnl*np.dot(B,u_sq) 
+Bu_cpu *= -1.
+assert(np.linalg.norm(Bu - Bu_cpu)<=1.e-10)
